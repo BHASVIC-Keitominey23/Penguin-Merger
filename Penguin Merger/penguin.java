f92467558381964bd4f53ddd0667d203ee9c72a2
BHASVIC-Keitominey23 <145456903+BHASVIC-Keitominey23@users.noreply.github.com>
2024-10-11 10:00:08 +0100

    h
    s

--- Penguin Merger/penguin.java
+++ Penguin Merger/penguin.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; 
 
 
 
@@ -6,11 +6,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class penguin extends Actor
 {
     private int mergepoints;
-    private int xspeed=0;
-    private int yspeed= 2;
-    private double gravity=1.5;
+    private int xspeed=0000;
+    private int yspeed=0000;
+    private double friction=16;
+    private double gravity=800;
     private int penglv;
     private boolean falling=true;
+    private boolean dropped=false;
     public penguin(int spenglv, int smergepoints)
     {
         mergepoints=smergepoints;
@@ -21,20 +23,45 @@ public class penguin extends Actor
     
     public void act()
     {
-        
-        setLocation(getX() + (int)xspeed, getY() + (int)yspeed);
+        //if(dropped){
+        setLocation(getX() + (int)xspeed/1000, getY() + (int)yspeed/1000);
         checkcollision();
         if(falling){
             yspeed += gravity;
         }
-        if(falling==false){
+        else if(falling==false){
             yspeed = 0;
+            if(xspeed>0){
+            xspeed -= friction;
         }
+        }
+    //}
     }
     public void checkcollision(){
+        World world = getWorld();
+        if(penglv==1){
+            Actor penguin1 = getOneIntersectingObject(penguinlv1.class);
+            if(penguin1 != null){
+                world.addObject(new penguinlv2(),((this.getX()+(penguin1.getX())/2,((this.getY())+(penguin1.getY()))/2;
+                world.removeObject(this);
+                world.removeObject(penguin1);
+            }
+            
+        }
+        
         if(getY()>340){
             falling = false;
         }
+        if(getX()>480){
+            setLocation(480,getY());
+            xspeed*=-0.7; 
+        }
+        if(getX()<120){
+            setLocation(120,getY());
+            xspeed*=-0.7; 
+            
+        }
+        
     
     }
 }
