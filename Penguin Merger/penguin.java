07627f3ba39ae68a6218a1f4018e28143c16ae2d
BHASVIC-Keitominey23 <145456903+BHASVIC-Keitominey23@users.noreply.github.com>
2024-10-13 18:58:47 +0100

    Update penguin.java

--- Penguin Merger/penguin.java
+++ Penguin Merger/penguin.java
@@ -40,14 +40,67 @@ public class penguin extends Actor
     public void checkcollision(){
         World world = getWorld();
         Actor penguin1 = getOneIntersectingObject(penguinlv1.class);
+        Actor penguin2 = getOneIntersectingObject(penguinlv2.class);
+        Actor penguin3 = getOneIntersectingObject(penguinlv3.class);
+        Actor penguin4 = getOneIntersectingObject(penguinlv4.class);
+        Actor penguin5 = getOneIntersectingObject(penguinlv5.class);
+        Actor penguin6 = getOneIntersectingObject(penguinlv6.class);
+        Actor penguin7 = getOneIntersectingObject(penguinlv7.class);
+        Actor penguin8 = getOneIntersectingObject(penguinlv8.class);
+        Actor penguin9 = getOneIntersectingObject(penguinlv9.class);
+
         if((penglv==1)&&(penguin1 != null)){
             
                 world.removeObject(penguin1);
                 world.addObject(new penguinlv2(),getX(),getY());
                 world.removeObject(this);
+            }
+        else if((penglv==2)&&(penguin2 != null)){
             
-                
-                
+                world.removeObject(penguin2);
+                world.addObject(new penguinlv3(),getX(),getY());
+                world.removeObject(this);
+            }
+        else if((penglv==3)&&(penguin3 != null)){
+            
+                world.removeObject(penguin3);
+                world.addObject(new penguinlv4(),getX(),getY());
+                world.removeObject(this);
+            }
+        else if((penglv==4)&&(penguin4 != null)){
+            
+                world.removeObject(penguin4);
+                world.addObject(new penguinlv5(),getX(),getY());
+                world.removeObject(this);
+            }
+        else if((penglv==5)&&(penguin5 != null)){
+            
+                world.removeObject(penguin5);
+                world.addObject(new penguinlv6(),getX(),getY());
+                world.removeObject(this);
+            }
+        else if((penglv==6)&&(penguin6 != null)){
+            
+                world.removeObject(penguin6);
+                world.addObject(new penguinlv7(),getX(),getY());
+                world.removeObject(this);
+            }
+        else if((penglv==7)&&(penguin7 != null)){
+            
+                world.removeObject(penguin7);
+                world.addObject(new penguinlv8(),getX(),getY());
+                world.removeObject(this);
+            }
+        else if((penglv==8)&&(penguin8 != null)){
+            
+                world.removeObject(penguin8);
+                world.addObject(new penguinlv9(),getX(),getY());
+                world.removeObject(this);
+            }
+        else if((penglv==9)&&(penguin9 != null)){
+            
+                world.removeObject(penguin9);
+                world.removeObject(this);
             }
             
         
