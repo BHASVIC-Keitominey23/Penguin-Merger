b73380a830dfe16835813cd03b42a8b61d602aff
BHASVIC-Keitominey23 <keito.miney23@students.bhasvic.ac.uk>
2024-12-06 13:06:56 +0000

    ye

--- Penguin Merger/rankingsmenu.java
+++ Penguin Merger/rankingsmenu.java
@@ -8,7 +8,10 @@ public class rankingsmenu extends World
     public rankingsmenu()
     {    
         super(600, 400, 1);
-        addObject( new remenubutton(),540,380);
+        addObject( new remenubutton(),540,370);
+        GreenfootImage newImage = new GreenfootImage("aice.png"); 
+        newImage.scale(600, 400); 
+        setBackground(newImage);
         
     }
 }
